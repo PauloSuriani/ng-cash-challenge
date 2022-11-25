9b4f0738a1f049301b12a4fe7b1762444d7ca485
PauloSuriani <paulosuriani@gmail.com>
2022-11-25 11:07:13 -0300

    add: sequelize config file

---
+++ backend/sequelize.js
@@ -0,0 +1,12 @@
+// "use strict";
+const dotenv = require ('dotenv');
+dotenv.config();
+
+module.exports = {
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database:process.env.POSTGRES_DB || 'postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    dialect: process.env.POSTGRES_DIALECT || 'postgres',
+    port: process.env.POSTGRES_PORT || 5430,
+};
